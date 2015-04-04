d87548a64770980693699342933be4aecc195a55
disissid <siddharth.kumar185@gmail.com>
2015-04-05 02:05:42 +0530

    New landing Page

--- public/javascripts/directives/directives.js
+++ public/javascripts/directives/directives.js
@@ -8,6 +8,8 @@ directives.directive('login',function(){
     }
 });
 
+
+/*allow only whole numbers to be input*/
 directives.directive('onlyDigits', function () {
     return {
         require: 'ngModel',
@@ -27,5 +29,22 @@ directives.directive('onlyDigits', function () {
             }
             ctrl.$parsers.push(inputValue);
         }
+    };
+});
+
+/*changes the value of boolChangeClass if scrolled down certain height*/
+
+directives.directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
+        angular.element($window).bind("scroll", function() {
+            if (this.pageYOffset >= 100) {
+                scope.boolChangeClass = true;
+                console.log('Scrolled below header.');
+            } else {
+                scope.boolChangeClass = false;
+                console.log('Header is in view.');
+            }
+            scope.$apply();
+        });
     };
 });
